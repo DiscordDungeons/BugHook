428f12086f0477d3ce234bca40659e3ca07715e0
Mackan <Thormax10@gmail.com>
2019-05-20 15:14:22 +0200

    Project works

--- src/index.js
+++ src/index.js
@@ -3,13 +3,24 @@ const chalk = require('chalk')
 
 const {
 	getProjects,
+	createCard,
+	getColumns,
 } = require('./services/GitHub.service')
+
+const WebhooksApi = require('@octokit/webhooks')
+
+const isDev = process.env.NODE_ENV !== 'production'
+
+const EventSource = require('eventsource')
 
 const checkEnvVars = () => {
 	const requiredVars = [
-		'GH_SECRET',
+		'GH_ACCESS_TOKEN',
 		'GH_PROJECT_REPO',
 		'GH_PROJECT_OWNER',
+		'GH_WEBHOOK_SECRET',
+		'GH_ISSUE_REPO_OWNER',
+		'GH_ISSUE_REPO_NAME',
 	]
 
 	const optionalVars = [
@@ -31,18 +42,16 @@ const checkEnvVars = () => {
 	})
 }
 
-const main = async () => {
+const getProject = async () => {
 	const { data, ok } = await getProjects({
 		owner: process.env.GH_PROJECT_OWNER,
 		repo: process.env.GH_PROJECT_REPO,
-		token: process.env.GH_SECRET,
+		token: process.env.GH_ACCESS_TOKEN,
 	})
 
 	if (!ok) {
 		console.log(chalk.redBright(`Error fetching repository: ${data.message}`))
 	} else {
-		console.log("Got repos", data)
-
 		if (data.length <= 0) {
 			console.log(chalk.redBright(`No projects found. Application will exit.`))
 			process.exit(0)
@@ -69,9 +78,102 @@ const main = async () => {
 			projectData = matchingProjects[0]
 		}
 
-		const { id: projectId } = projectData
+		return projectData
+	}
+}
 
-		console.log("Project ID", projectId)
+const getColumn = async (projectId) => {
+	const { data, ok } = await getColumns({
+		projectId,
+		token: process.env.GH_ACCESS_TOKEN,
+	})
+
+	if (!ok) {
+		console.log(chalk.redBright(`Error fetching columns: ${data.message}`))
+	} else {
+		if (data.length <= 0) {
+			console.log(chalk.redBright(`No columns found. Application will exit.`))
+			process.exit(0)
+		}
+
+		let columnData
+
+		if (!process.env.GH_PROJECT_COLUMN) {
+			columnData = data[0]
+		} else {
+			const columnName = process.env.GH_PROJECT_COLUMN.toLowerCase()
+
+			let matchingColumns = data.filter(column => column.name.toLowerCase() === columnName)
+		
+			if (matchingColumns.length <= 0) {
+				console.log(chalk.redBright(`No columns found with name ${columnName}. Application will exit.`))
+				process.exit(0)
+			}
+
+			if (matchingColumns.length > 1) {
+				console.warn(chalk.yellowBright(`Multiple columns with name ${columnName} found. Using first occurance.`))
+			}
+
+			columnData = matchingColumns[0]
+		}
+
+		return columnData
+	}
+}
+
+const main = async () => {
+	const { id: projectId } = await getProject()
+
+	console.log('Project ID:', projectId)
+
+	const { id: columnId } = await getColumn(projectId)
+
+	console.log('Column ID:', columnId)
+
+	const webhooks = new WebhooksApi({
+		secret: process.env.GH_WEBHOOK_SECRET,
+	})
+
+	webhooks.on('*', async ({ name, payload }) => {
+		if (name === 'issues') {
+			const {
+				action,
+				issue: {
+					number: issueNumber,
+				},
+			} = payload
+
+			if (action === 'opened') {
+				const { data, ok } = await createCard({
+					columnId,
+					token: process.env.GH_ACCESS_TOKEN,
+					note: `${process.env.GH_ISSUE_REPO_OWNER}/${process.env.GH_ISSUE_REPO_NAME}#${issueNumber}`,
+				})
+
+				if (ok) {
+					console.log(chalk.green(`Successfully created card for issue ${issueNumber}`))
+				} else {
+					console.error(chalk.redBright(`Couldn't create card for issue ${issueNumber}: ${data.message}`))
+				}
+			}
+		}
+	})
+
+	if (!isDev) {
+		require('http').createServer(webhooks.middleware).listen(3000)
+	} else {
+		const source = new EventSource(process.env.WEBHOOK_PROXY_URL)
+
+		source.onmessage = (event) => {
+			const webhookEvent = JSON.parse(event.data)
+
+			webhooks.verifyAndReceive({
+				id: webhookEvent['x-request-id'],
+				name: webhookEvent['x-github-event'],
+				signature: webhookEvent['x-hub-signature'],
+				payload: webhookEvent.body,
+			}).catch(console.error)
+		}
 	}
 }
 
--- src/services/GitHub.service.js
+++ src/services/GitHub.service.js
@@ -1,6 +1,8 @@
 const fetch = require('node-fetch')
 
-const getProjects = ({ owner, repo, token }) => fetch(`https://api.github.com/repos/${owner}/${repo}/projects`, {
+const baseURL = 'https://api.github.com'
+
+const getProjects = ({ owner, repo, token }) => fetch(`${baseURL}/repos/${owner}/${repo}/projects`, {
 	headers: {
 		'Accept': 'application/vnd.github.inertia-preview+json',
 		'Authorization': `token ${token}`,
@@ -12,6 +14,40 @@ const getProjects = ({ owner, repo, token }) => fetch(`https://api.github.com/re
 	ok: r.ok,
 }))
 
+const getColumns = ({ projectId, token }) => fetch(`${baseURL}/projects/${projectId}/columns`, {
+	headers: {
+		'Accept': 'application/vnd.github.inertia-preview+json',
+		'Authorization': `token ${token}`,
+	},
+}).then(async r => ({
+	data: await r.json(),
+	statusCode: r.status,
+	statusText: r.statusText,
+	ok: r.ok,
+}))
+
+const createCard = ({
+	columnId,
+	token,
+	note,
+}) => fetch(`${baseURL}/projects/columns/${columnId}/cards`, {
+	method: 'POST',
+	headers: {
+		'Accept': 'application/vnd.github.inertia-preview+json',
+		'Authorization': `token ${token}`,
+	},
+	body: JSON.stringify({
+		note,
+	}),
+}).then(async r => ({
+	data: await r.json(),
+	statusCode: r.status,
+	statusText: r.statusText,
+	ok: r.ok,
+}))
+
 module.exports = {
 	getProjects,
+	getColumns,
+	createCard,
 }
